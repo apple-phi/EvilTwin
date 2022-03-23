bbd562ba83fbb520c71135f087dde3b57e7b4de7
LiquidPulsar <noelalexandre66@gmail.com>
2022-03-23 20:40:31 +0000

    Enemy subclass stuff

--- EvilTwin/player.py
+++ EvilTwin/player.py
@@ -2,10 +2,9 @@ import numpy as np
 from tilemap import Tile
 
 class Player:
-    def __init__(self, map, coords, is_enemy = False):
+    def __init__(self, map, coords):
         self.map = map
         self.xy = coords
-        self.is_enemy = is_enemy
         self.dest = self.src
 
         self.is_moving = False
@@ -16,8 +15,6 @@ class Player:
         Chooses the player's destination in the x and/or y direction based on a vector
         ranging from [-1,-1] to [1,1]
         """
-        if self.is_enemy:
-            xy = (-xy[0], -xy[1])
 
         mov = self.xy
 
@@ -61,3 +58,7 @@ class Player:
 
     def __repr__(self):
         return f"Player - Current: {self.xy}, Dest: {self.dest}, Dir: {self.dir}, moving: {self.is_moving}"
+
+class Enemy(Player):
+    def move(self,xy):
+        return super().move(-xy[0],-xy[1])
