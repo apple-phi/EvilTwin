1d51402a1a3ff50e41d18ba2ff115c7e2642bf46
LiquidPulsar <noelalexandre66@gmail.com>
2022-03-24 00:16:22 +0000

    Yeehaw

--- EvilTwin/__init__.py
+++ EvilTwin/__init__.py
@@ -1,39 +1,98 @@
 import pathlib
 import sys
+from random import randint
+from typing import Callable
+
 import pygame
 
+from .constants import LEVELS, TILE_SIZE
 from .levels import Level
-from .constants import TILE_SIZE, LEVELS
 from .player import Player
+
+LOADING, IN_MENU, IN_LEVEL = 0, 1, 2
+MOVES ={
+    pygame.K_w: (0, -1),
+    pygame.K_a: (-1, 0),
+    pygame.K_s: (0, 1),
+    pygame.K_d: (1, 0),
+}
+
+class Button:
+    def __init__(self, x: int, y: int, w: int, h: int, command: Callable):
+        self.x,self.y,self.w,self.h,self.command = x,y,w,h,command
+        self.image = pygame.Surface((TILE_SIZE, TILE_SIZE))
+        col = [0, 0, 0]
+        col[randint(0,2)] = 255
+        self.image.fill(col)
+
+
+    @staticmethod
+    def run(buttons: list["Button"], x, y):
+        for b in buttons:
+            if b.x <= x <= b.x + b.w and b.y <= y <= b.y + b.h:
+                b.command()
+    def display(self,screen : pygame.Surface):
+        screen.blit(
+            pygame.transform.scale(self.image, (self.w, self.h)),
+            (self.x, self.y),
+        )
+
 
 
 class Game:
     def __init__(self):
         screen = pygame.display.set_mode((640, 480), pygame.SCALED)
-        level = Level(LEVELS / "test.toml")
-
-        level.show_on(screen)
+        self.level = Level(LEVELS / "test.toml")
+        self.state = IN_MENU
+        self.level.show_on(screen)
         clock = pygame.time.Clock()
 
-        self.pl = Player(level, (0, 0))
+        self.pl = Player(self.level, self.level.start)
+
+        self.menu = [
+            Button(
+                x,
+                50,
+                TILE_SIZE,
+                TILE_SIZE,
+                lambda n=n: self.enter_level(f"level{n}.toml")
+            )
+            for n,x in enumerate(range(0, 1000, TILE_SIZE * 2),1)
+        ]
+
         while True:
-            for event in pygame.event.get():
-                match event.type:
-                    case pygame.QUIT:
-                        pygame.quit()
-                        sys.exit()
-                    case pygame.KEYDOWN:
-                        self.parse_event(event.key)
-
-
-            screen.fill((0, 0, 0))
-            level.show_on(screen)
-            self.pl.move()
-            self.pl.show_on(screen)
+            self.check_events()
+            if self.state == IN_MENU:
+                for b in self.menu:
+                    b.display(screen)
+            elif self.state == IN_LEVEL:
+                screen.fill((0, 0, 0))
+                self.level.show_on(screen)
+                self.pl.move()
+                self.pl.show_on(screen)
 
             pygame.display.flip()
             clock.tick(30)
-    
+
     def parse_event(self, key):
-        if key == pygame.K_s:
-            self.pl.path((0, 1))
+        if self.state == IN_LEVEL:
+            if key == pygame.K_ESCAPE:
+                self.state = IN_MENU
+            elif key in MOVES:
+                self.pl.path(MOVES[key])
+
+    def check_events(self):
+        for event in pygame.event.get():
+            match event.type:
+                case pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                case pygame.KEYDOWN:
+                    self.parse_event(event.key)
+                case pygame.MOUSEBUTTONDOWN:
+                    if self.state == IN_MENU:
+                        Button.run(self.menu,*pygame.mouse.get_pos())
+    
+    def enter_level(self,level):
+        self.state = IN_LEVEL
+        self.level = Level(LEVELS / level)
