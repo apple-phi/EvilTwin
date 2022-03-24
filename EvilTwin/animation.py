fc6d67b682bb4218bf832f8041fb69560a680374
lkn849 <lkn849@gmail.com>
2022-03-24 21:10:30 +0000

    make standalone images into infinite iterator

--- EvilTwin/animation.py
+++ EvilTwin/animation.py
@@ -4,7 +4,7 @@ import pygame
 
 
 class SpriteAnimation:
-    """Single images are stores as pygame images;
+    """Single images are stores as infinite iterators of pygame images;
     Animations are stored as an `itertools.cycle` of pygame images."""
 
     def __init__(self, path):
@@ -13,7 +13,8 @@ class SpriteAnimation:
             self.load_file(entry) if entry.is_file() else self.load_group(entry)
 
     def load_file(self, file: pathlib.Path):
-        self.animations[file.stem] = pygame.image.load(file)
+        surface = pygame.image.load(file)
+        self.animations[file.stem] = iter(lambda: surface, None)
 
     def load_group(self, directory: pathlib.Path):
         images = [pygame.image.load(entry) for entry in directory.iterdir()]
