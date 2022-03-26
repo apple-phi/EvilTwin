bc4fee7bf4a41f8eee3b3ba124acfd2dca231536
lkn849 <lkn849@gmail.com>
2022-03-26 22:20:14 +0000

    Add better cursor and icon

--- EvilTwin/__init__.py
+++ EvilTwin/__init__.py
@@ -3,25 +3,25 @@ import sys
 import pygame
 
 pygame.init()
-pygame.mixer.music.set_volume(.15)
+pygame.mixer.music.set_volume(0.15)
 
 from .constants import *
 from .scenes import TitleScreen
 
 
+CURSOR = pygame.transform.smoothscale(
+    pygame.image.load(ASSETS / "cursor.png"), (40, 40)
+)
+pygame.mouse.set_visible(False)
+
+
 class Game:
     pygame.mouse.set_visible(False)
+
     def __init__(self):
         self.screen = pygame.display.set_mode((700, 700), pygame.SCALED)
-
-        self.curs = pygame.transform.flip(pygame.transform.scale(pygame.image.load(TILES / "cursor.png").convert_alpha(), (16, 16)), True, False)
-        
-        w, h = self.curs.get_size()
-        for x in range(w):
-            for y in range(h):
-                self.curs.set_at((x, y), pygame.Color(150, 0, 255, self.curs.get_at((x, y))[3]))
         pygame.display.set_caption("Mirror Mirror")
-        pygame.display.set_icon(STAR_SPRITE)
+        pygame.display.set_icon(pygame.image.load(ASSETS / "cute_star.png"))
         self.scene = TitleScreen()
 
     def run(self):
@@ -31,7 +31,7 @@ class Game:
             if self.scene.next_scene is not None:
                 self.scene = self.scene.next_scene
             self.scene.show_on(self.screen)
-            self.screen.blit(self.curs, pygame.mouse.get_pos())
+            self.screen.blit(CURSOR, pygame.mouse.get_pos())
             pygame.display.flip()
             clock.tick(40)
 
